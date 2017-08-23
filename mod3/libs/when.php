a188e50841ced4f04006f4cf00882486a700192a
Jessica Vicentini <jvicentini@tray.net.br>
2017-08-23 11:19:25 -0300

    Exercicios-mod3

--- mod3/libs/when.php
+++ mod3/libs/when.php
@@ -1,7 +1,5 @@
 <?php
 require __DIR__ . ('/date.php');
 
-echo 'Hoje é ' . dataHoje() . ', ontem foi ' .
- dataOntem() . ', amãmha será ' .
- dataAmanha() . '. Se eu salvar isso no banco, os valores serão ' .
- dataBanco() . PHP_EOL;
+echo 'Hoje é ' . dataHoje() . ', ontem foi ' . dataOntem() . ', amãmha será ' . 
+dataAmanha() . '. Se eu salvar isso no banco, os valores serão ' . dataBanco() . PHP_EOL;
